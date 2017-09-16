8a5028d9a503ad8006fe2c608a4308c539ffd750
IlievskiV <ilievski.vladimir@live.com>
2017-09-17 00:48:37 +0200

    Change the frontend

--- server/thecombiners/thecombiners/settings.py
+++ server/thecombiners/thecombiners/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'thecombiners.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-}
+#DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 DATABASES['default'] = dj_database_url.parse('postgres://thecombiners_user:mojkombajn@localhost:5432/thecombiners_db', conn_max_age=600)
 
